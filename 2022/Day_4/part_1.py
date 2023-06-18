0f3bc80c7e767b0c2aa95b1285bc40d859cadd13
Fangolinn <36115121+Fangolinn@users.noreply.github.com>
2023-06-18 21:06:07 +0200

    Day 4 & 5

---
+++ 2022/Day_4/part_1.py
@@ -0,0 +1,31 @@
+# https://adventofcode.com/2022/day/4
+
+from collections import namedtuple
+
+
+Range = namedtuple("Range", ["start", "end"])
+
+def parse_line(line: str) -> tuple[Range, Range]:
+    line = line.strip().split(",")
+
+    boundries1, boundries2 = line[0].split("-"), line[1].split("-")
+    return (Range(int(boundries1[0]), int(boundries1[1])), Range(int(boundries2[0]), int(boundries2[1])))
+
+def range_contains_the_other(range1: Range, range2: Range) -> bool:
+    if range1.start <= range2.start and range1.end >= range2.end:
+        return True
+    
+    if range2.start <= range1.start and range2.end >= range1.end:
+        return True
+    
+    return False
+
+count: int = 0
+
+with open("./2022/Day_4/input.txt") as input_file:
+    for line in input_file.readlines():
+        range1, range2 = parse_line(line)
+        count += int(range_contains_the_other(range1, range2))
+
+print("Result:", count)
+        
---
+++ 2022/Day_5/part_2.py
@@ -0,0 +1,57 @@
+# https://adventofcode.com/2022/day/5#part2
+
+# https://adventofcode.com/2022/day/5
+
+from pathlib import Path
+import textwrap
+
+input_file = open(Path(__file__).parent / "input.txt")
+
+starting_stacks = []
+while True:
+    line = input_file.readline()
+
+    if line == "\n":
+        break
+
+    starting_stacks.append(line)
+
+starting_stacks_parsed = []
+
+for level in starting_stacks[-2::-1]:
+    starting_stacks_parsed.append(level[1::4])
+
+stacks_amount = len(starting_stacks_parsed[0])
+stacks: list[list] = [[] for _ in range(stacks_amount)]
+
+for level in starting_stacks_parsed:
+    for item, stack_no in zip(level, range(stacks_amount)):
+        stacks[stack_no].append(item) if item != " " else None
+
+def move_item(command: str):
+    """
+    Expected command format: 
+        'move n from x to y'
+
+        n - amount of items to move
+        x - source stack
+        y - destination stack
+    """
+    cmd = command.split(" ")
+    amount, start_stack, dest_stack = int(cmd[1]), int(cmd[3]), int(cmd[5])
+
+    items = []
+
+    for _ in range(amount):
+        items.append(stacks[start_stack - 1].pop())
+
+    items.reverse()
+    
+    stacks[dest_stack - 1] += items
+
+for line in input_file.readlines():
+    move_item(line)
+    
+print("".join([stack[-1] for stack in stacks]))
+
+input_file.close()
